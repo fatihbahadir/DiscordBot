f1bf181d9ec0a299efe4eca9ca3bff0123b29401
orkun kurul <orkun.kurul@hotmail.com>
2022-03-07 21:03:08 +0300

    cogs update

--- DiscordBot/cogs/Management.py
+++ DiscordBot/cogs/Management.py
@@ -6,28 +6,31 @@ class Manager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command()
-@commands.has_role("tenkstu")
-async def kick(ctx, member: discord.Member, *args, reason="Yok"):
-    await member.kick(reason=reason)
-    await ctx.send(prettify(f"The {member.display_name} kicked by me."))
+    @commands.command()
+    @commands.has_role("tenkstu")
+    async def kick(ctx, member: discord.Member, *args, reason="Yok"):
+        await member.kick(reason=reason)
+        await ctx.send(prettify(f"The {member.display_name} kicked by me."))
 
-@commands.command()
-@commands.has_role("tenkstu")
-async def ban(ctx, member: discord.Member, *args, reason="Yok"):
-    await member.ban(reason=reason)
-    await ctx.send(prettify(f"The {member.display_name} banned by me."))
+    @commands.command()
+    @commands.has_role("tenkstu")
+    async def ban(ctx, member: discord.Member, *args, reason="Yok"):
+        await member.ban(reason=reason)
+        await ctx.send(prettify(f"The {member.display_name} banned by me."))
 
-@commands.command()
-@commands.has_role("tenkstu")
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split("#")
+    @commands.command()
+    @commands.has_role("tenkstu")
+    async def unban(ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split("#")
 
-    for bans in banned_users:
-        user = bans.user
+        for bans in banned_users:
+            user = bans.user
 
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(prettify(f'{user.display_name} has no longer banned'))
-            return
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(prettify(f'{user.display_name} has no longer banned'))
+                return
+
+def setup(bot):
+    bot.add_cog(commands(bot))
