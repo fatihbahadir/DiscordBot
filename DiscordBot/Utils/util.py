e0ab02ca2aada25187cbb89b3c958c5385e41ffe
orkun kurul <orkun.kurul@hotmail.com>
2022-03-07 23:38:48 +0300

    Update util.py

--- DiscordBot/Utils/util.py
+++ DiscordBot/Utils/util.py
@@ -2,5 +2,5 @@ def prettify(text):
     return "```"+str(text)+"```"
 
 def create_list(title, items):
-    message = prettify(title.upper()+"\n".join(["- "+i for i in items]))
+    message = prettify(title.upper()+"\n"+"\n".join(["- "+i for i in items]))
     return message
