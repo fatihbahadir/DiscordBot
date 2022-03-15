71db457846474d1d75d60d5764e95fa332d5af33
orkun kurul <orkun.kurul@hotmail.com>
2022-03-15 14:29:21 +0300

    update fun.py management.py
    - Hungman Game Imported
    
      * Class imported
      * Functions created and utialized
    
    - mis_channels function improved
    
      * seperated by private or not

--- DiscordBot/cogs/fun.py
+++ DiscordBot/cogs/fun.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 from Utils.util import prettify,create_list
 from random import randint
 import random
-import requests
-from bs4 import BeautifulSoup
+import os
+
 class Game:
 
     @staticmethod
@@ -22,6 +22,68 @@ class Game:
         lane=["top","jungle","mid","adc","support"]
         selected=lane[randint(0,len(lane)-1)]
         return selected
+
+
+class Hungman:
+    def __init__(self, word=None):
+        
+        self.word = word
+        self.run = True
+        
+        self.word_meta = []
+        self.stages = {}
+        self.wrong_letters = []
+
+        self.win = False
+
+    def change_letter(self, letter):
+        any_letter = 0
+        for l in self.word_meta:
+            if l[0] == letter.lower():
+                l[1] = True
+                any_letter += 1
+
+        if not any_letter:
+            self.wrong_letters.append(letter)
+
+    def get_curr_word(self):
+        res = "".join([l[0] if l[1] else "_" for l in self.word_meta])
+        return res
+
+    def get_curr_hung_stat(self):
+        lenght = len(self.wrong_letters)
+        return self.stages.get("stage"+str(lenght), "")
+
+    def get_wrong_list(self):
+        return "WRONG LIST: " + ",  ".join(self.wrong_letters)
+
+    def check_meta(self):
+        return all([i[1] for i in self.word_meta])
+
+    def check_game_finished(self):
+        if self.check_meta():
+            self.win = True
+            self.run = False
+
+        if len(self.wrong_letters) > 6:
+            self.run = False
+
+    def init(self):
+        
+        # Init hungman pattern
+        folder_loc = "Data/hungsman_stages/"
+        for text_file in [f for f in os.listdir(folder_loc) if f.endswith(".txt")]:
+            text_file_name = text_file[:-4]
+            with open(folder_loc+text_file, "r") as txt_file:
+                self.stages[text_file_name] = txt_file.read()
+
+        # Init word meta
+        for letter in self.word:
+            self.word_meta.append([letter,False])
+
+    @staticmethod
+    def check_letter(letter):
+        return len(letter) == 1 and letter.isalpha()
 
 class Fun(commands.Cog,name="Fun Commands"):
     def __init__(self,bot):
@@ -60,7 +122,7 @@ class Fun(commands.Cog,name="Fun Commands"):
 
         beer_offer = f"{user.name}, you got a 🍺 from {ctx.author.name}"
         beer_offer = beer_offer + f"\n\nReason: {reason}" if reason else beer_offer
-        msg = await ctx.send(prettify(beer_offer))
+        await ctx.send(prettify(beer_offer))
 
     @commands.command(description="Press F to pay respect")
     async def f(self,ctx, *, text: commands.clean_content = None):
@@ -99,18 +161,41 @@ class Fun(commands.Cog,name="Fun Commands"):
         else:
             await ctx.send(prettify(f"Nope it was {computer}"))
     
-    @commands.command(description="Bitcoin Price")
-    async def btcprice(self,ctx):
-       url=requests.get("https://coinmarketcap.com/")
-       soup= BeautifulSoup(url.content,features="html.parser")
-       tagBtc=soup.findAll("a",{"href":"/currencies/bitcoin/markets/"})
-       cntr=0
-       for btc in tagBtc:
-           price=btc.text
-           await ctx.send(prettify(f"BTC price is {price} USD"))
-           cntr +=1
-           if cntr ==1:
-                break
+    @commands.command(description="Play Hangman With Bot")
+    async def hungman(self, ctx):
+        
+        if ctx.channel.name != "game":
+            await ctx.send(prettify("You run this command only in 'GAME' channel"))
+            return
+        
+        a = Hungman("kaplan")
+        a.init()
+
+        def check(msg):
+            return msg.author == ctx.author and msg.channel == ctx.channel
+
+        await ctx.send(prettify("Welcome to Hungman"))
+
+        while a.run:
+            await ctx.send(prettify("letter >> "))
+            msg = await self.bot.wait_for("message", check=check)
+            letter = msg.content
+
+            if not a.check_letter(letter):
+                await ctx.send(prettify(f"Input must be character. (WRONG: {letter})"))
+                continue
+
+            a.change_letter(letter)
+            a.check_game_finished()
+            if a.run:
+                await ctx.send(prettify(a.get_curr_word()))
+                await ctx.send(prettify(a.get_curr_hung_stat()))
+                await ctx.send(prettify(a.get_wrong_list()))
+
+        if a.win:
+            await ctx.send(prettify("You win the game"))
+        else:
+            await ctx.send(prettify("You lose the game"))
 
 def setup(bot):
     bot.add_cog(Fun(bot))
